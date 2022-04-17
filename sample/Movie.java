3f6b6c5accc11ac6a81f19a66602e6ac23fe7b31
NotCameron <cfisher9098@gmail.com>
2022-04-17 17:11:35 -0400

    Inital commit

---
+++ sample/Movie.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class Movie {
+    private String title, genre;
+    private Date time;
+    private Integer duration;
+}
---
+++ sample/Theater.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Theater {
+    private ArrayList<Movie> movies;
+    private Integer roomNum, capacity;
+}
